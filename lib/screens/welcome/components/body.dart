adf0bc6309ab6a00fd2d16147786e7e77e5a4292
Jai Shiv <nsoni19012001@gmail.com>
2020-09-07 00:46:46 +0530

    fix : SignUp UI changed.

--- lib/screens/welcome/components/body.dart
+++ lib/screens/welcome/components/body.dart
@@ -11,54 +11,56 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context)
         .size; // this thing gives whole screen total height & width of our screen
-    return Background(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'WELCOME',
-              style: TextStyle(
-                  color: kPrimaryColor,
-                  fontWeight: FontWeight.w700,
-                  fontSize: 28.0,
-                  letterSpacing: 2),
-            ),
-            Text(
-              'KR TRADING COMPANY',
-              style: TextStyle(
-                  color: kPrimaryColor,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 18.0,
-                  letterSpacing: 1.2),
-            ),
-            SizedBox(
-              height: size.height * 0.05,
-            ),
-            SvgPicture.asset(
-              'assets/icons/chat.svg',
-              height: size.height * 0.45,
-            ),
-            SizedBox(
-              height: size.height * 0.05,
-            ),
-            RoundedButton(
-              text: 'LOGIN',
-              press: () {
-                Navigator.pushNamed(context, SignInScreen.routeName);
-              },
-            ),
-            RoundedButton(
-              text: 'Sign Up',
-              press: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return SignUpScreen();
-                }));
-              },
-              textColor: Colors.black,
-              color: kPrimaryLightColor,
-            ),
-          ],
+    return SafeArea(
+      child: Background(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'WELCOME',
+                style: TextStyle(
+                    color: kPrimaryColor,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 28.0,
+                    letterSpacing: 2),
+              ),
+              Text(
+                'KR TRADING COMPANY',
+                style: TextStyle(
+                    color: kPrimaryColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18.0,
+                    letterSpacing: 1.2),
+              ),
+              SizedBox(
+                height: size.height * 0.05,
+              ),
+              SvgPicture.asset(
+                'assets/icons/chat.svg',
+                height: size.height * 0.45,
+              ),
+              SizedBox(
+                height: size.height * 0.05,
+              ),
+              RoundedButton(
+                text: 'LOGIN',
+                press: () {
+                  Navigator.pushNamed(context, SignInScreen.routeName);
+                },
+              ),
+              RoundedButton(
+                text: 'Sign Up',
+                press: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return SignUpScreen();
+                  }));
+                },
+                textColor: Colors.black,
+                color: kPrimaryLightColor,
+              ),
+            ],
+          ),
         ),
       ),
     );
